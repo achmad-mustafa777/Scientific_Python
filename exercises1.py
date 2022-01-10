d93ecd4f7eeca0b7cd19cdb14228e10b71d0239c
achmad-mustafa777 <achmadmustafa777@gmail.com>
2022-01-10 11:51:43 +0800

    VARIABLES, EXPRESSIONS, AND STATEMENTS

---
+++ exercises1.py
@@ -0,0 +1,30 @@
+# Exercise 2: Write a program that uses input to prompt a user for their
+# name and then welcomes them.
+
+# name = input('enter your name: ')
+# print('Welcome ' + name)
+
+# Exercise 3: Write a program to prompt the user for hours and rate per
+# hour to compute gross pay.
+
+# haours = int(input('Enter hours: '))
+# rates = int(input('enter rates: '))
+# pays = haours * rates
+
+# print('Gajih yang harus dibayar $', pays)
+
+# Exercise 5: Write a program which prompts the user for a Celsius tem-
+# perature, convert the temperature to Fahrenheit, and print out the
+# converted temperature.
+
+# rumusnya (°C × 9/5) + 32 = °F
+
+# print('conversi celcius ke fahrenhit')
+
+# celcius = float(input('masukan nilainya: '))
+
+# fahrenhit = (celcius * 9/5) + 32
+
+# print('hasilnya: ', fahrenhit)
+
+# conversi sirup ke drop
